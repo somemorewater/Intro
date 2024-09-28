f66e2542f34ab295367a5b8b47b7c86a31b6d202
somemorewater <oboyiloejeh@gmail.com>
2024-09-28 01:05:51 +0000

    update

--- models/User.js
+++ models/User.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
+    fullName: { type: String, required: true },
     username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    dateOfBirth: { type: Date, required: true },
+    phone: { type: String },
+    location: { type: String },
+    bio: { type: String },
+    profilePicture: { type: String }, // URL or path to the profile picture
+    coverPhoto: { type: String }, // URL or path to the cover photo
 });
 
 const User = mongoose.model('User', UserSchema);
