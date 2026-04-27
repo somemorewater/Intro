69f6b435c5415be8c355fdf096aaa491d6333c40
Water <oboyiloejeh@gmail.com>
2026-04-27 17:10:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use('/uploads', express.static('uploads'));
